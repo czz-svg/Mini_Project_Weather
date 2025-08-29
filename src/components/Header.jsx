4fe8c21c8ffc6025793d26e6b084ff62d52a259b
Viet <cozozo2000@gmail.com>
2025-08-29 20:06:29 +0700

    add logo in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 export default function Header(){
     return(
-        <>
+        <div className="header">
+        <img src="img/sun.png" alt="logo-website" />
         <h1>WELCOME <em>WEATHER</em> WEBSITE</h1>
-        </>
+        </div>
     )
 }
